1f52c70bc305d7d6d912b408832c2cd45a50616f
Jessica Garnaud <42799703+Lutheen@users.noreply.github.com>
2022-11-24 12:01:58 +0100

    templates fixed

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -17,7 +17,7 @@ class AppFixtures extends Fixture
         $faker = Factory::create();
 
         $users = [];
-        for ($i=0; $i < 50; $i++) { 
+        for ($i=0; $i < 10; $i++) { 
             $user = (new User())->setUsername($faker->userName())
                                 ->setEmail($faker->email())
                                 ->setPassword(sha1('password'))
@@ -28,7 +28,7 @@ class AppFixtures extends Fixture
             $manager->persist($user);
         }
 
-        $categories = [
+        $names = [
             "Immobilier",
             "Véhicules",
             "Vacances",
@@ -40,40 +40,32 @@ class AppFixtures extends Fixture
             "Matériel professionnel",
             "Autres"
         ];
-        for ($i=0; $i < count($categories); $i++) { 
-            $category = (new Category())->setName($categories[$i])
-                                        ->setDescription("Description de :".$categories[$i])
-                                        ->setImage($faker->imageUrl(200, 200, $categories[$i], true))
+        $categories = [];
+        for ($i=0; $i < count($names); $i++) { 
+            $category = (new Category())->setName($names[$i])
+                                        ->setDescription("Description de :".$names[$i])
+                                        ->setImage($faker->imageUrl(200, 200, $names[$i], true))
             ;
+            $categories[] = $category;
             $manager->persist($category);
         }
 
-        // $categories = [];
-        // for ($i=0; $i < 10; $i++) { 
-        //     $category = (new Category())->setName($faker->word())
-        //                                 ->setDescription($faker->sentence(3))
-        //                                 ->setImage($faker->imageUrl(200, 200, 'category', true))
-        //     ;
-        //     $categories[] = $category;
-        //     $manager->persist($category);
-        // }
-
-        $images = [];
         $products = [];
         for ($i=0; $i < 25; $i++) { 
             $product = (new Product())->setName($faker->sentence(3))
                                       ->setDescription($faker->text(50))
                                       ->setPrice($faker->randomFloat(2, 1, 100))
                                       ->setCreatedAt($faker->dateTime())
-                                      ->addImage($images[rand(0, count($images)-1)])
                                       ->setCategory($categories[rand(0, count($categories)-1)])
                                       ->setSeller($users[rand(0, count($users)-1)])
             ;
             $products[] = $product;
             $manager->persist($product);
+
         }
 
-        for ($i=0; $i < 50; $i++) { 
+        $images = [];
+        for ($i=0; $i < 50; $i++) {
             $image = (new Image())->setImage($faker->imageUrl(700, 500, 'image', true))
                                   ->setProduct($products[rand(0, count($products)-1)])
             ;
