b6e33990751c2ac8c7eb60150b3339a97389b915
Jessica Garnaud <42799703+Lutheen@users.noreply.github.com>
2022-11-29 12:16:31 +0100

    css on account page

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,13 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/profile')]
+#[Route('/account')]
 class ProfileController extends AbstractController
 {
-    #[Route('/', name: 'app_profile')]
+    #[Route('/', name: 'app_account')]
     public function index(): Response
     {
         return $this->render('profile/index.html.twig', [
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+
+    #[Route('/profile', name: 'app_profile')]
+    public function profile(): Response
+    {
+        return $this->render('profile/profile.html.twig', [
             'controller_name' => 'ProfileController',
         ]);
     }
