325db5f90552e47d5650bcbb4244930430869565
Jessica Garnaud <42799703+Lutheen@users.noreply.github.com>
2023-01-16 18:09:12 +0100

    twig extension for active links in navbar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,12 +25,13 @@ class HomeController extends AbstractController
         ]);
     }
 
-    public function header(CategoryRepository $categoryRepository): Response
+    public function header(CategoryRepository $categoryRepository, RequestStack $requestStack): Response
     {
         $categories = $categoryRepository->findAll();
 
         return $this->render('partials/header.html.twig', [
             'categories' => $categories,
+            'route' => $requestStack->getParentRequest()->getPathInfo()
         ]);
     }
 }
